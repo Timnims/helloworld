0a7e790935151306d1a09fde4d7ce980b9d59117
Timnims <106060011+Timnims@users.noreply.github.com>
2024-06-18 11:04:31 -0700

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,43 @@
+#include <iostream> 
+using namespace std;
+//COMMENT XD
+
+/*
+multi
+LINE
+COMMENT!!
+*/
+// >> = extraction operator
+// << = insertion operator
+// cout = console out (screen)
+
+int main(){  // ALL PROGRAMS MUST HAVE FUNCTION "MAIN"
+
+    // int = What function returns, main = name of function
+
+    cout<<"Hello World!"<<endl; // PRINTS "Hello World!"
+
+
+    //std::cout<<"Hello World!"<<std::end1; //if we do nut use namespace std above
+    return 0;
+
+}
+/*
+ g++ -c HelloWorld.cpp 
+ g++ -o HelloWorld HelloWorld.o
+ ./HelloWorld
+
+ OR
+
+    g++ -o HelloWorld  HelloWorld.cpp
+or
+    g++ HelloWorld.cpp -o HelloWorld
+
+then ./HelloWorld
+
+
+
+
+
+echo $? = exit status of last command
+*/
